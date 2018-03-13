dcf177f4946bd171b55b4911f838186fdf059f3d
Michael MacLeod <michaelmmacleod@gmail.com>
2018-03-12 20:58:09 -0700

    Add documentation

--- args.lisp
+++ args.lisp
@@ -6,11 +6,18 @@
 
 (in-package :macleod.args)
 
+;;; Drops elements off the start of xs until it reaches str
 (defun drop-until-string= (str xs)
   (loop for x on xs
         when (string= str (car x))
         return x))
 
+;;; Matches xs against arg-list, binding any symbols in arg-list.
+;;;
+;;; The following initializes `topic' with the string "my-command"
+;;;
+;;;     (with-match '("--help" "my-command") ("--help" topic)
+;;;       (format t "Here's some help with ~a~%" topic))
 (defmacro with-match (xs arg-list &body body)
   (let* ((arg-match (car arg-list))
          (params-match (cdr arg-list))
@@ -21,6 +28,7 @@
                    collect `(,param-match ,param))
          ,@body))))
 
+;;; Matches command line arguments against arg-list, see `with-match' for details.
 (defmacro with-arg (arg-list &body body)
   `(with-match sb-ext:*posix-argv* ,arg-list
      ,@body))

5cf50b2f254f7aec4067af3b5e545edf54f0577e
Michael MacLeod <michaelmmacleod@gmail.com>
2018-03-12 20:48:53 -0700

    Implement with-match and with-arg

--- args.lisp
+++ args.lisp
@@ -1,46 +1,26 @@
 (in-package :cl-user)
 
-(defpackage :michaelmmacleod.args
+(defpackage :macleod.args
   (:use :common-lisp)
-  (:export :with-args))
+  (:export :with-arg))
 
-(in-package :michaelmmacleod.args)
+(in-package :macleod.args)
 
-(defun argument-p (x)
-  (and (> (length x) 2)
-       (string-equal x "--" :end1 2)
-       (subseq x 2)))
-
-(defun get-args ()
-  (cdr sb-ext:*posix-argv*))
-
-(defun make-arg (x)
-  (format nil "--~a" x))
-
-(defun collect-until (test xs)
-  (loop for x in xs
-        if (funcall test x)
-            return ys
-        else
-            collect x into ys
-        finally (return ys)))
-
-(defun drop-until (test xs)
+(defun drop-until-string= (str xs)
   (loop for x on xs
-        when (funcall test (first x))
+        when (string= str (car x))
         return x))
 
-(defun get-parameters-for (argument xs)
-  (collect-until 
-    #'argument-p
-    (cdr (drop-until
-           #'(lambda (x) (string-equal argument x))
-           xs))))
+(defmacro with-match (xs arg-list &body body)
+  (let* ((arg-match (car arg-list))
+         (params-match (cdr arg-list))
+         (params (drop-until-string= arg-match (eval xs))))
+    (if params
+      `(let ,(loop for param in (cdr params)
+                   for param-match in params-match
+                   collect `(,param-match ,param))
+         ,@body))))
 
-(defmacro with-args ((&rest syms) &body body)
-  `(let ,(mapcar #'(lambda (x)
-                     (let ((arg (make-arg (symbol-name x)))
-                           (cmd-args (get-args)))
-                       `(,x (get-parameters-for ,arg ',cmd-args))))
-                 syms)
+(defmacro with-arg (arg-list &body body)
+  `(with-match sb-ext:*posix-argv* ,arg-list
      ,@body))

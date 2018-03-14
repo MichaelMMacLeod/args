e6650582ec7918fc509a33912e96379d093353bc
Michael MacLeod <michaelmmacleod@gmail.com>
2018-03-13 19:58:05 -0700

    nil when matching differently sized list
    Before, with ./test-program --help arg1 arg2
    and
    
    (with-args ("--help" sym1)
    
    (with-args ("--help" sym1 sym2)
    
    would both of the above cases (the first one just ignores the
    second argument).
    
    Now only the second case will run; the first just expanding to nil.

--- args.lisp
+++ args.lisp
@@ -14,19 +14,20 @@
 ;;;
 ;;; nil if the lists do not line up or match properly.
 (defmacro with-matches (args args-match &body body)
-  (block outer
-    `(let ,(loop with matches = 0
-                 for arg in (eval args)
-                 for arg-match in args-match
-                 if (symbolp arg-match)
-                   collect `(,arg-match ,arg) into xs and
-                   do (incf matches)
-                 do (and (stringp arg-match) (string= arg-match arg)
-                         (incf matches))
-                 finally (if (= matches (length args-match))
-                           (return xs)
-                           (return-from outer nil)))
-       ,@body)))
+  (if (= (length (eval args)) (length args-match))
+    (block outer
+      `(let ,(loop with matches = 0
+                   for arg in (eval args)
+                   for arg-match in args-match
+                   if (symbolp arg-match)
+                     collect `(,arg-match ,arg) into xs and
+                     do (incf matches)
+                   do (and (stringp arg-match) (string= arg-match arg)
+                           (incf matches))
+                   finally (if (= matches (length args-match))
+                             (return xs)
+                             (return-from outer nil)))
+         ,@body))))
 
 ;;; Automatically supplies command line arguments to with-matches
 (defmacro with-args (args-match &body body)
